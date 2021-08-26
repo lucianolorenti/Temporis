159466581422906bc4c739ee7d3802bfbbf6c350
Luciano Lorenti <lucianolorenti@gmail.com>
2021-08-26 17:37:19 +0200

    Dataset: Precompute common features and durations in the Abstract Dataset class

--- temporis/dataset/ts_dataset.py
+++ temporis/dataset/ts_dataset.py
@@ -8,16 +8,20 @@ import pandas as pd
 
 
 class AbstractTimeSeriesDataset:
+    def __init__(self):
+        self._common_features = None
+        self._durations = None
 
     @property
     def n_time_series(self):
-        raise NotImplementedError 
+        raise NotImplementedError
 
     """Base class of the dataset handled by this library.
 
         Methods for fitting and transform receives an instance
         that inherit from this class
     """
+
     def get_time_series(self, i: int) -> pd.DataFrame:
         """
 
@@ -28,7 +32,6 @@ class AbstractTimeSeriesDataset:
         """
         raise NotImplementedError
 
-
     def durations(self) -> List[float]:
         """Obtain the length of each life
 
@@ -37,9 +40,9 @@ class AbstractTimeSeriesDataset:
         List[float]
             List of durations
         """
-        return [
-            life[self.rul_column].iloc[0] for life in self
-        ]
+        if self._durations is None:
+            self._durations = [life[self.rul_column].iloc[0] for life in self]
+        return self._durations
 
     def __getitem__(self, i: Union[int, Iterable]):
         """Obtain a time-series or an splice of the dataset using a FoldedDataset
@@ -60,14 +63,15 @@ class AbstractTimeSeriesDataset:
             pd.DataFrame: the i-th time-series
             FoldedDataset: The dataset with the lives specified by the list
         """
+
         if isinstance(i, Iterable):
             if not all(isinstance(item, (int, np.integer)) for item in i):
-                raise ValueError('Invalid iterable index passed')
+                raise ValueError("Invalid iterable index passed")
 
             return FoldedDataset(self, i)
         else:
             df = self.get_time_series(i)
-            df['life'] = i
+            df["life"] = i
             return df
 
     @property
@@ -83,7 +87,7 @@ class AbstractTimeSeriesDataset:
         """
         return self.n_time_series
 
-    def toPandas(self, proportion=1.0) -> pd.DataFrame:
+    def to_pandas(self, proportion=1.0) -> pd.DataFrame:
         """
         Create a dataset with the time-series concatenated
 
@@ -99,7 +103,7 @@ class AbstractTimeSeriesDataset:
             Return a DataFrame with all the lives concatenated
         """
         df = []
-        common_features = self.commonFeatures()
+        common_features = self.common_features()
         for i in range(self.n_time_series):
             if proportion < 1.0 and np.random.rand() > proportion:
                 continue
@@ -107,17 +111,21 @@ class AbstractTimeSeriesDataset:
             df.append(current_life)
         return pd.concat(df)
 
-    def commonFeatures(self) -> List[str]:
-        f = []
-        for i in range(self.n_time_series):
-            life = self[i]
-            f.append(set(life.columns.values))
-        return f[0].intersection(*f)
-
+    def common_features(self) -> List[str]:
+        if self._common_features is None:
+            self._common_features = []
+            for i in range(self.n_time_series):
+                life = self[i]
+                self._common_features.append(set(life.columns.values))
+            self._common_features = self._common_features[0].intersection(
+                *self._common_features
+            )
+        return self._common_features
 
 
 class FoldedDataset(AbstractTimeSeriesDataset):
     def __init__(self, dataset: AbstractTimeSeriesDataset, indices: list):
+        super().__init__()
         self.dataset = dataset
         self.indices = indices
 
