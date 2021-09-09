554c8720271b944c0b004478955d14c85b4e426d
Luciano Lorenti <lucianolorenti@gmail.com>
2021-09-10 00:40:12 +0200

    Transformation: Concat dataframes with pd.concat to avoid fragmentation

--- temporis/transformation/functional/concatenate.py
+++ temporis/transformation/functional/concatenate.py
@@ -9,20 +9,18 @@ from temporis.transformation.functional.mixin import TransformerStepMixin
 
 
 class Concatenate(TransformerStepMixin):
-
-
     def merge_dataframes_by_column(self, Xs):
         # indices = [X.index for X in Xs]
         # TODO: Check equal indices
         names = [n.name for n in self.previous]
-        X = Xs[0]
-        X.columns = [f'{names[0]}_{c}' for c in X.columns]
-        for name, otherX in zip(names[1:], Xs[1:]):
-            for c in otherX.columns:
-                X[f'{name}_{c}'] = otherX[c]
+
+        X = pd.concat(
+            [otherX.add_prefix(f'{name}_{i}_') for i, (name, otherX) in enumerate(zip(names, Xs))], axis=1
+        )
+
         return X
 
-    def transform(self, Xs:List[pd.DataFrame]):
+    def transform(self, Xs: List[pd.DataFrame]):
         if not Xs:
             # All transformers are None
             return np.zeros((Xs[0].shape[0], 0))
