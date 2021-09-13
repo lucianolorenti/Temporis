b009357f80f830bc0cf8983253bb063e28751ec7
Luciano Lorenti <lucianolorenti@gmail.com>
2021-09-13 17:11:33 +0200

    Iterators: ALlow true_values to accept a dataset. A default iterator will be contructed

--- temporis/iterators/utils.py
+++ temporis/iterators/utils.py
@@ -1,3 +1,7 @@
+
+from temporis.transformation.functional.transformers import TransformerIdentity
+from numpy.lib.arraysetops import isin
+from temporis.dataset.ts_dataset import AbstractTimeSeriesDataset
 from typing import Union
 
 import numpy as np
@@ -5,13 +9,14 @@ from temporis.iterators.batcher import Batcher
 from temporis.iterators.iterators import WindowedDatasetIterator
 
 
-
-def true_values(dataset_iterator: Union[WindowedDatasetIterator, Batcher]) -> np.array:
+def true_values(
+    dataset_iterator: Union[WindowedDatasetIterator, Batcher, AbstractTimeSeriesDataset]
+) -> np.array:
     """Obtain the true RUL of the dataset after the transformation
 
     Parameters
     ----------
-    dataset_iterator : Union[WindowedDatasetIterator, Batcher]
+    dataset_iterator : Union[WindowedDatasetIterator, Batcher, AbstractTimeSeriesDataset]
         Iterator of the dataset
 
     Returns
@@ -20,6 +25,13 @@ def true_values(dataset_iterator: Union[WindowedDatasetIterator, Batcher]) -> np
          target values after the transformation
     """
     if isinstance(dataset_iterator, Batcher):
-        dataset_iterator = dataset_iterator.iterator 
-    d =  np.concatenate([y for _, y, _ in dataset_iterator])
+        dataset_iterator = dataset_iterator.iterator
+    if isinstance(dataset_iterator, AbstractTimeSeriesDataset):
+        t = TransformerIdentity()
+        t.fit(dataset_iterator)
+
+        dataset_iterator = WindowedDatasetIterator(
+            dataset_iterator, window_size=1, transformer=t
+        )
+    d = np.concatenate([y for _, y, _ in dataset_iterator])
     return d
