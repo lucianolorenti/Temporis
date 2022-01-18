e92b56b6bac2d3abdf24e10a8424406094eafa4e
Luciano Lorenti <lucianolorenti@gmail.com>
2022-01-18 12:12:42 +0100

    Dataset: Add Serializable transformed dataset

--- temporis/dataset/transformed.py
+++ temporis/dataset/transformed.py
@@ -13,7 +13,7 @@ from temporis.utils.lrucache import LRUDataCache
 
 class TransformedDataset(AbstractTimeSeriesDataset):
     def __init__(self, dataset, transformer:Transformer, cache_size:Optional[int]=None):
-        self.transformer = transformer 
+        self.transformer = transformer
         self.dataset = dataset
         if cache_size is None:
             cache_size = len(dataset)
@@ -22,12 +22,7 @@ class TransformedDataset(AbstractTimeSeriesDataset):
 
 
     @property
-    def n_time_series(self):
-        """Base class of the dataset handled by this library.
-
-            Methods for fitting and transform receives an instance
-            that inherit from this class
-        """
+    def n_time_series(self) -> int:
         return self.dataset.n_time_series
 
     def __call__(self, i:int):
@@ -37,7 +32,7 @@ class TransformedDataset(AbstractTimeSeriesDataset):
         _, y, _ = self[i]
         return y.shape[0]
 
-        
+
     def get_time_series(self, i: int) -> pd.DataFrame:
         """
 
@@ -56,16 +51,41 @@ class TransformedDataset(AbstractTimeSeriesDataset):
         TransformedSerializedDataset.save(self, output_path)
 
 
-class TransformedSerializedDataset(AbstractTimeSeriesDataset):
+class TransformedSerializedDataset(TransformedDataset):
     @staticmethod
     def save(dataset:TransformedDataset, output_path:Path):
         for i, life in enumerate(dataset):
-            with gzip.open(output_path / 'ts_{i}.pkl.gz', 'wb') as file:            
+            with gzip.open(output_path / f'ts_{i}.pkl.gz', 'wb') as file:
                 pickle.dump(life, file)
         with open(output_path / 'transformer.pkl.gz', 'wb') as file:
-            pickle.dump(file, dataset.transformer)
-        
-    def __init__(self, dataset_path:Path):
+            pickle.dump(dataset.transformer, file)
+
+    def __init__(self, dataset_path:Path, cache_size:Optional[int] = None):
+        self.dataset_path = dataset_path
         self.files = list(dataset_path.glob('ts_*.pkl.gz'))
+        with open(dataset_path / 'transformer.pkl.gz', 'rb') as file:
+            self.transformer = pickle.load(file)
+        if cache_size is None:
+            cache_size = len(self.files)
+        self.cache = LRUDataCache(cache_size)
 
+    def _open_file(self, i:int):
+        with gzip.open(self.dataset_path / self.files[i], 'rb') as file:
+            return pickle.load(file)
 
+    def get_time_series(self, i: int) -> pd.DataFrame:
+        """
+
+        Returns
+        -------
+        pd.DataFrame
+            DataFrame with the data of the life i
+        """
+        if i not in self.cache.data:
+            X, y, metadata = self._open_file(i)
+            self.cache.add(i, (X, y, metadata))
+        return self.cache.get(i)
+
+    @property
+    def n_time_series(self):
+        return len(self.files)
