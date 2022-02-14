0c9081d5080d948871566fe7467a4f9119395394
Luciano Lorenti <lucianolorenti@gmail.com>
2022-02-14 11:02:31 +0100

    Resamplers: Update the resamplers module

--- temporis/transformation/features/resamplers.py
+++ temporis/transformation/features/resamplers.py
@@ -1,46 +1,72 @@
+import numpy as np
 import pandas as pd
-from sklearn.base import BaseEstimator, TransformerMixin
+from scipy.interpolate import interp1d
+from temporis.transformation import TransformerStep
 
 
-class ResamplerTransformer(BaseEstimator, TransformerMixin):
-    def __init__(self, time_feature, time='40s', interpolation_method='linear', enabled=True):
-        self.time = time
-        self.interpolation_method = interpolation_method
-        self.time_feature = time_feature
-        self.enabled = enabled
+class IntegerIndexResamplerTransformer(TransformerStep):
+    """IntegerIndexResamplerTransformer
 
-    def fit(self, X, y=None):
+    Resample the time series with an integer index and interpolate linearly the values
+
+    Parameters
+    ----------
+    time_feature : str
+        Time feature
+    steps : int
+        Number of steps
+    drop_time_feature: bool
+        Drop the time feature
+    """
+
+    def __init__(self, *args, time_feature: str, steps: int, drop_time_feature: bool):
+        super().__init__(*args)
+        self._time_feature_name = time_feature
+        self._time_feature = None
+        self.steps = steps
+        self.drop_time_feature = drop_time_feature
+
+    def partial_fit(self, X: pd.DataFrame):
+        """Obtain the name of the feature used as time
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            The current time-series to be fitted
+
+        Returns
+        -------
+        self
+
+        """
+        if self._time_feature is None:
+            self._time_feature = self.find_feature(X, self._time_feature_name)
         return self
 
-    def transform(self, df):
+    def transform(self, X: pd.DataFrame):
+        Told = X[self._time_feature].values
+        Tnew = np.arange(Told.min(), Told.max(), self.steps)
+        new_columns = X.columns.values
+        if self.drop_time_feature:
+            new_columns = np.delete(
+                new_columns, np.where(new_columns == self._time_feature)
+            )
 
-        if self.enabled:
-            X = df.copy()
-            X[self.time_feature] = pd.to_timedelta(
-                X[self.time_feature], unit='s')
-            X = (X
-                 .drop_duplicates(subset=self.time_feature)
-                 .set_index(self.time_feature)
-                 # .resample('5s', origin='start')
-                 # .mean()
-                 .resample(self.time, origin='start')
-                 .interpolate()
-                 .reset_index())
-            X[self.time_feature] = X[self.time_feature].astype('int')
-            return X
-        else:
-            return df
-
-
-class SubSampleTransformer(BaseEstimator, TransformerMixin):
-    def __init__(self, step: int):
-        self.step = step
-
-    def fit(self, X, y=None):
-        return self
-
-    def partial_fit(self, X, y=None):
-        return self
-
-    def transform(self, df):
-        return df.iloc[range(0, df.shape[0], self.step), :].copy()
+        new_X = pd.DataFrame(index=Tnew, columns=new_columns)
+        for c in new_columns:
+            try:
+                F = interp1d(Told, X[c])
+                new_X[c] = F(Tnew)
+            except ValueError:
+                # https://stackoverflow.com/questions/62015823/interpolating-categorical-data-in-python-nearest-previous-value/62016097#62016097
+                y = X[c].values
+                f = interp1d(
+                    Told,
+                    range(len(y)),
+                    kind="nearest",
+                    fill_value=(0, len(y) - 1),
+                    bounds_error=False,
+                )
+                y_idx = f(Tnew)
+                new_X[c] = [y[int(i)] for i in y_idx]
+        return new_X
